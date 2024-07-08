0657c0a757658a52021e33f95c803a3212f0d282
Venkat-015 <venkat15003sridhar@gmail.com>
2024-07-08 19:20:31 +0530

    form actions

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,4 +1,27 @@
+import { Box,Typography,useMediaQuery,useTheme } from "@mui/material";
+import Form from "./form.jsx";
 const LoginPage=()=>{
-    return <div>loginpage</div>;
-};
+    const theme=useTheme();
+    const isNonMobileScreens=useMediaQuery("(min-width:1000px)");
+
+    return (
+        <Box>
+        <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+            <Typography fontWeight="bold"
+            fontSize="32px"
+            color="primary"
+            >Blaze</Typography>
+    </Box>
+    <Box width={isNonMobileScreens?"50%":"93%"} 
+    p="2rem"
+    m="2rem auto"
+    borderRadius="1.5rem"
+    backgroundColor={theme.palette.background.alt}>
+        <Typography fontWeight="500" variant="h5" sx={{mb:"1.5rem"}}>
+            Welcome to <em>Blaze!</em> The Socio Blogging Website...
+        </Typography>
+        <Form />
+    </Box>
+    </Box>
+);};
 export default LoginPage;
