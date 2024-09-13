9a449cdf00661240d07f2d3421d094770e41bbb1
Venkat Narayan <venkat15003sridhar@gmail.com>
2024-09-13 09:51:19 +0530

    blaze

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Blaze
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
