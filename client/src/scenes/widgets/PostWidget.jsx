d10444c4667aab07e3a7109fe2cd1133d34e3257
Venkat Narayan <venkat15003sridhar@gmail.com>
2024-09-16 15:01:52 +0530

    ss

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,4 @@
+import { ChatBubbleOutlineOutlined,FavoriteBorderOutlined,FavoriteOutlined,ShareOutlined } from "@mui/icons-material";
+import { Box,IconButton,Divider,Typography,useTheme} from "@mui/material";
+import FlexBetween from "components/flexbetween";
+import Friend from "components/Friend";
